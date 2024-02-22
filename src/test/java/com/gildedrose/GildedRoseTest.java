8c7330d08f1894c1944d5faafec02c771eb57549
CLEBC54 <caroline.leveaux@realdolmen.com>
2024-02-22 08:37:21 +0100

    testCase before refactoring

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,11 +9,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void updateQuality() {
+        CombinationApprovals.verifyAllCombinations(
+                this::doUpdateItem,
+                new String[]{"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
+                new Integer[]{-1, 0, 2, 6, 11},
+                new Integer[]{0, 1, 49, 50});
+    }
+    private String doUpdateItem(String name, int sellIn, int quality) {
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        return app.items[0].toString();
     }
-
 }
